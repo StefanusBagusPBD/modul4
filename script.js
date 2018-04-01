03c7382358da80343c20e4612b79f8ac36cc1c82
StefanusBagusPBD <stevanusbagus123@gmail.com>
2018-04-01 19:53:29 +0700

    tugas modul 4

---
+++ script.js
@@ -0,0 +1,30 @@
+var heightJohn = 170;
+var heightMike = 195;
+var heightBudi = 200;
+var ageJohn = 28;
+var ageMike = 38;
+var ageBudi = 40;
+var nama1 = 'John';
+var nama2 = 'Mike';
+var nama3 = 'Budi';
+
+var menang1 = ageJohn * 5 + 170;
+var menang2 = ageMike * 5 + 195;
+var menang3 = ageBudi * 5 + 200;
+
+console.log(menang3);
+console.log(menang2);
+console.log(menang1);
+
+
+
+function yearsUntilRetirement(name, year) {}
+{
+var pengumuman = ageBudi - ageMike;
+    
+    if (pengumuman >= 0) {
+        console.log(nama3 + ' adalah Pemenang 1 ' + menang3 + ' skornya');
+    } else {
+        console.log(nama1 + ' adalah pemenang 1 ' + menang1 + 'skornya');
+    }
+}
